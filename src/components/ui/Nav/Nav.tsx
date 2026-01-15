cac7b803574c77970639be78375963a13bdbf77c
Santino Ventrice <santino.ventrice@gmail.com>
2026-01-15 04:14:00 -0300

    Update Nav and styles

--- src/components/ui/Nav/Nav.tsx
+++ src/components/ui/Nav/Nav.tsx
@@ -1,19 +1,25 @@
+"use client";
+
 // Styles
 import styles from "./Nav.module.css";
 
-// Components
-import SignOutButton from "../../auth/SignOutButton";
+// Context
+import { useUser } from "@/context/AuthContext";
 
 export default function Nav() {
+  const User = useUser();
   return (
     <header className={styles.nav}>
       <a href="/">
-        <h1>MyApp</h1>
+        <h1>Binge Quest</h1>
       </a>
       <nav>
         <ul>
           <li>
-            <SignOutButton />
+            <a href="https://github.com/SantiVentri/binge-quest" target="_blank">See repository</a>
+          </li>
+          <li>
+            <a href="/profile">{User?.user_metadata.display_name || User?.email || "Profile"}</a>
           </li>
         </ul>
       </nav>
