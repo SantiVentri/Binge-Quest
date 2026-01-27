5ac1630a46e83e873bae10ec192241205ac34741
Santino Ventrice <santino.ventrice@gmail.com>
2026-01-27 09:43:15 -0300

    Add games section to home page
    Introduces a new games section to the home page with a grid layout, individual game cards, and a 'coming soon' notice. Updates CSS for layout and hover effects, and adds a foreground hover color variable to globals.css.

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,50 @@
+// Styles
+import Link from "next/link";
 import styles from "./page.module.css";
+
+const games = [
+  {
+    title: "Trivia Game",
+    image: "/images/games/trivia_game.png",
+    slug: "trivia-game"
+  },
+  {
+    title: "Guess The Film",
+    image: "/images/games/guess_the_film.png",
+    slug: "guess-the-film"
+  },
+  {
+    title: "Find The Impostor",
+    image: "/images/games/find_the_impostor.png",
+    slug: "find-the-impostor"
+  },
+]
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Welcome to my App</h1>
+        <section className={styles.games}>
+          <div className={styles.gameList}>
+            {games.map((game) => (
+              <Link
+                key={game.title}
+                className={styles.gameCard}
+                href={`/games/${game.slug}`}
+              >
+                <img
+                  className={styles.gameImage}
+                  src={game.image}
+                  alt={game.title}
+                />
+                <h2 className={styles.gameTitle}>{game.title}</h2>
+              </Link>
+            ))}
+          </div>
+          <div className={styles.comingSoon}>
+            <h2>More games coming soon!</h2>
+          </div>
+        </section>
       </main>
     </div>
   );
